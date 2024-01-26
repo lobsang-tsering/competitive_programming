e88407a2d0cf563d2e2c9e9de7edaacfb80624fa
lobsang tsering <iamlobsangtsering@gmail.com>
2024-01-26 22:13:59 +0530

    class files added

---
+++ string_and_array/recursive/IBH.java
@@ -0,0 +1,18 @@
+import java.util.function.*;
+
+class IBH {
+  public static void print(int n) {
+
+    if(n == 1) {  
+      System.out.println(1);
+      return;
+    }
+
+    System.out.println(n);
+    print(n-1);
+
+  }
+  public static void main(String[] args) {
+    print(7);
+  }
+}
